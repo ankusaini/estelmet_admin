9ce4c498bb85d611b5bd35eead540bd1c850c3da
sanchit khurana <sanchit.khurana@dawnbit.com>
2020-01-17 08:39:06 +0530

    create pc half

--- src/app/modules/purchase/pages/purchase-confirmation/create-pc/create-pc.component.ts
+++ src/app/modules/purchase/pages/purchase-confirmation/create-pc/create-pc.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Purchase } from "src/app/shared/Models/purchase.model";
+import { PurchaseService } from "src/app/modules/purchase/services/purchase.service";
+import { Product } from "src/app/shared/Models/product.model.";
 
 @Component({
-  selector: 'app-create-pc',
-  templateUrl: './create-pc.component.html',
-  styleUrls: ['./create-pc.component.scss']
+  selector: "app-create-pc",
+  templateUrl: "./create-pc.component.html",
+  styleUrls: ["./create-pc.component.scss"]
 })
 export class CreatePcComponent implements OnInit {
+  showData: boolean;
+  public productList: Product[] = [];
 
-  constructor() { }
+  constructor(private purchaseService: PurchaseService) {}
+  public selectedMr: Purchase;
+  ngOnInit() {}
 
-  ngOnInit() {
+  getSelectedMr(data) {
+    this.selectedMr = data;
+    console.log("MR SELECTEd", this.selectedMr);
+    this.getProductList(this.selectedMr.id);
   }
 
+  getProductList(id) {
+    let url = "/purchase/find/" + id;
+    this.purchaseService.findRequstObjectById(url).subscribe(
+      data => {
+        this.productList = data.productList;
+                console.log("product of this mr",this.productList)
+
+        this.showData = true;
+      },
+      error => {
+        console.log("error");
+      }
+    );
+  }
 }
