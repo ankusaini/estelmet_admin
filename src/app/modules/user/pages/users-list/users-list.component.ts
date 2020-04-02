0d974f5b8ddff1def9767f29b496d8410b7c7b1f
Sakshi <sakshi.doomra@dawnbit.com>
2020-04-02 17:25:45 +0530

    pdf changes

--- src/app/modules/user/pages/users-list/users-list.component.ts
+++ src/app/modules/user/pages/users-list/users-list.component.ts
@@ -7,6 +7,11 @@ import { User } from "src/app/shared/Models/user.model";
 import { Observable } from "rxjs/internal/Observable";
 import { Subject } from "rxjs";
 import { UserDataService } from 'src/app/shared/services/data/userData.service';
+
+import * as jspdf from 'jspdf'; 
+ 
+import html2canvas from 'html2canvas';
+
 // import * as xlsx from 'xlsx';
 @Component({
   selector: "app-users-list",
@@ -170,6 +175,23 @@ export class UsersListComponent implements OnInit, AfterViewInit {
     // const wb: xlsx.WorkBook = xlsx.utils.book_new();
     // xlsx.utils.book_append_sheet(wb, ws, 'Sheet1');
     // xlsx.writeFile(wb, 'epltable.xlsx');
+console.log("fnc")
+
+    var data = document.getElementById('contentToConvert');
+html2canvas(data).then(canvas => {
+// Few necessary setting options
+var imgWidth = 208;
+var pageHeight = 295;
+var imgHeight = canvas.height * imgWidth / canvas.width;
+var heightLeft = imgHeight;
+ 
+const contentDataURL = canvas.toDataURL('image/png')
+let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+var position = 0;
+pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+pdf.save('MYPdf.pdf'); // Generated PDF
+});
+
    }
    print() {
     window.print();
