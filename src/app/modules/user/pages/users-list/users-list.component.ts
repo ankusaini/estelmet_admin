d5bf5d9820c947e35823d7b0fbd0fcc5ed850986
Ankit <ankit@anaadigsoft.com>
2020-04-09 08:57:23 +0530

    excel

--- src/app/modules/user/pages/users-list/users-list.component.ts
+++ src/app/modules/user/pages/users-list/users-list.component.ts
@@ -7,8 +7,8 @@ import { UserDataService } from 'src/app/shared/services/data/userData.service';
 import { UserService } from 'src/app/shared/services/user.service';
 import Swal from 'sweetalert2';
 import { ids } from 'src/app/shared/Models/ids.model';
-// import * as jspdf from 'jspdf'; 
-// import html2canvas from 'html2canvas';
+import * as jspdf from 'jspdf'; 
+import html2canvas from 'html2canvas';
 // import * as xlsx from 'xlsx';
 
 @Component({
@@ -140,19 +140,19 @@ export class UsersListComponent implements OnInit, AfterViewInit {
     console.log("fnc")
 
     var data = document.getElementById('contentToConvert');
-    // html2canvas(data).then(canvas => {
-    // // Few necessary setting options
-    // var imgWidth = 208;
-    // var pageHeight = 295;
-    // var imgHeight = canvas.height * imgWidth / canvas.width;
-    // var heightLeft = imgHeight;
+    html2canvas(data).then(canvas => {
+    // Few necessary setting options
+    var imgWidth = 208;
+    var pageHeight = 295;
+    var imgHeight = canvas.height * imgWidth / canvas.width;
+    var heightLeft = imgHeight;
     
-    // const contentDataURL = canvas.toDataURL('image/png')
-    // let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
-    // var position = 0;
-    // pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-    // pdf.save('MYPdf.pdf'); // Generated PDF
-    // });
+    const contentDataURL = canvas.toDataURL('image/png')
+    let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+    var position = 0;
+    pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+    pdf.save('MYPdf.pdf'); // Generated PDF
+    });
 
    }
    print() {
